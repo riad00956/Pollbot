65c8b382a930bf9ae33fb31c5b7c7672cea9a805
riad00956 <afrozshahana24@gmail.com>
2026-02-01 17:13:23 +0600

    Add bot configuration settings
    Add configuration settings for bot token, API URL, and super admins.

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "8565269885:AAGTq8Ju6uXjFX89nEEBu8CDRyrVcSOv9d8"
+WORKER_API_URL = "https://api-poll-bot.mdkabirulislam556677.workers.dev"
+SUPER_ADMINS = 7832264582 # Telegram IDs of super admins
